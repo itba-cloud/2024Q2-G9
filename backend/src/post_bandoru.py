4afb799a5d4a9873a5e574b52723f0d63df12d8f
RupnikF <francorupnik@gmail.com>
2024-10-22 12:07:27 -0300

    fix: index out of range in split

--- backend/src/post_bandoru.py
+++ backend/src/post_bandoru.py
@@ -18,7 +18,8 @@ app = APIGatewayHttpResolver(enable_validation=True, cors=cors_config, debug=Tru
 @tracer.capture_method
 def post_bandoru(form: CreateBandoruForm):
     auth_header = app.current_event.headers.get('Authorization', None)
-    token = auth_header.split(" ")[1] if auth_header else None
+    splitted = auth_header.split(" ") if auth_header else None
+    token = splitted[1] if splitted and len(splitted) > 1 else None
     username = get_username_from_token(token) if token else None
     return bandoru_service.create(form, username), 201
 
